65c753fc7d2bc73308a6175fcda0f08dfb933e37
Shaun Smith <shaun@agilecanada.com>
2018-11-06 16:44:06 +0000

    Update build

--- fnbuild.sh
+++ fnbuild.sh
@@ -3,12 +3,12 @@
 mkdir ${WERCKER_ROOT}/bin
 chmod -R 755 ${WERCKER_ROOT}/bin
 echo "bin dir"
-echo "$(ls -la ${WERCKER_ROOT})"
+echo "$(ls -la ${WERCKER_ROOT}/bin)"
 curl -L -o ${WERCKER_ROOT}/bin/fn https://github.com/fnproject/cli/releases/download/0.5.28/fn_linux
 echo "post download"
-echo "$(ls -la ${WERCKER_ROOT})"
+echo "$(ls -la ${WERCKER_ROOT}/bin)"
 chmod +x ${WERCKER_ROOT}/bin/fn
 echo "post-chmod"
-echo "$(ls -la ${WERCKER_ROOT})"
+echo "$(ls -la ${WERCKER_ROOT}/bin/fn)"
 ${WERCKER_ROOT}/bin/fn version
 ${WERCKER_ROOT}/bin/fn -verbose build
